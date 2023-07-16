7bbf1a0192e0de20a56d010ec6edffe3159758c9
Hectormra23 <88894715+hectormra23@users.noreply.github.com>
2023-07-16 11:05:57 -0600

    new update
    v. 4.0.01 "Scripts and css functions from "ïnfografias" and "index" update"

---
+++ Infografias/script_info.js
@@ -0,0 +1,4 @@
+
+ScrollReveal().reveal('.contenedor-imagen', { delay: 500 });
+ScrollReveal().reveal('.contenedor-imagen', { delay: 500 });
+ScrollReveal().reveal('.contenedor-imagen', { delay: 500 });
